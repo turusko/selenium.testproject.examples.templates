3daac1024829b7366ee23f1f8798c5f0854d5075
Chris Fisher <Turusko@gmail.com>
2025-02-22 20:01:54 +0000

    Fixed issue by using IClassFixture and ensuring the fixture is disposed off after tests run.

---
+++ Fixtures/AddOrRemoveElementsFixture.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using selenium.testproject.examples.templates.Drivers;
+using selenium.testproject.examples.templates.Enums;
+using selenium.testproject.examples.templates.StepDefinitions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace selenium.testproject.examples.templates.Fixtures
+{
+    public class AddOrRemoveElementsFixture : IDisposable
+    {
+        public readonly IWebDriver Driver;
+        public readonly AddOrRemoveElementsSteps Steps;
+
+        public AddOrRemoveElementsFixture() 
+        {
+
+            Driver =  new SeleniumDrivers(BrowserType.Edge).Driver;
+            Driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/add_remove_elements/");
+            Steps = new AddOrRemoveElementsSteps(Driver);
+
+        }
+
+        public void Dispose()
+        {
+            Driver.Quit();
+            Driver.Dispose();
+        }
+    }
+}
---
+++ Tests/AddOrRemoveElementsTests.cs
@@ -0,0 +1,25 @@
+﻿using selenium.testproject.examples.templates.Fixtures;
+using selenium.testproject.examples.templates.PageObjects;
+
+
+namespace selenium.testproject.examples.templates.Tests
+{
+    public class AddOrRemoveElementsTests : IClassFixture<AddOrRemoveElementsFixture>
+
+    {
+        private readonly AddOrRemoveElementsFixture _fixture;
+        public AddOrRemoveElementsTests(AddOrRemoveElementsFixture fixture) : base()
+        {
+            _fixture = fixture;
+
+        }
+        [Fact]
+        public void ClickingTheAddElementCreatesANewButton()
+        {
+            _fixture.Steps.GivenTheAddElementsButtonIsVisible();
+            _fixture.Steps.WhenIClickTheAddElementsButton();
+            _fixture.Steps.ThenTheDeleteButtonIsVisible();
+        }
+
+    }
+}

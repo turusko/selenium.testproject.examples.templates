624326001e776180b7662796493bcb35a1fe41d9
Chris Fisher <Turusko@gmail.com>
2025-02-22 19:19:34 +0000

    Task Have drivers for Chrome, Firefox, and Edge

---
+++ StepDefinitions/AddOrRemoveElementsSteps.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using OpenQA.Selenium;
+using selenium.testproject.examples.templates.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace selenium.testproject.examples.templates.StepDefinitions
+{
+    public class AddOrRemoveElementsSteps
+    {
+        private readonly AddOrRemoveElementsPage _page;
+        public AddOrRemoveElementsSteps(IWebDriver driver) 
+        {
+            _page = new AddOrRemoveElementsPage(driver);
+        }
+
+        public void GivenTheAddElementsButtonIsVisible()
+        {
+            _page.IsAddElementsButtonVisible().Should().BeTrue();
+        }
+        public void WhenIClickTheAddElementsButton()
+        {
+            _page.ClickAddElementsButton();
+        }
+
+        public void ThenTheDeleteButtonIsVisible()
+        {
+            _page.IsDeleteButtonVisible().Should().BeTrue();
+        }
+    }
+}
---
+++ Tests/AddOrRemoveElementsTests.cs
@@ -0,0 +1,21 @@
+﻿using selenium.testproject.examples.templates.Fixtures;
+using selenium.testproject.examples.templates.PageObjects;
+
+
+namespace selenium.testproject.examples.templates.Tests
+{
+    public class AddOrRemoveElementsTests : AddOrRemoveElementsFixture
+    {
+        public AddOrRemoveElementsTests() : base()
+        {
+
+        }
+        [Fact]
+        public void ClickingTheAddElementCreatesANewButton()
+        {
+            _steps.GivenTheAddElementsButtonIsVisible();
+            _steps.WhenIClickTheAddElementsButton();
+            _steps.ThenTheDeleteButtonIsVisible();
+        }
+    }
+}

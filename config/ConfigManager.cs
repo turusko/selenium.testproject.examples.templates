9cb39d308fd6dfc12c402dcf53669c03d2c50143
Christopher Fisher <christopher.fisher@conduent.com>
2025-02-26 09:17:19 +0000

    Added config support and some refactor

---
+++ config/ConfigManager.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace selenium.testproject.examples.templates.config
+{
+    public class ConfigManager
+    {
+        private static IConfigurationRoot _configuration;
+
+        static ConfigManager()
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", false, true);
+            _configuration = builder.Build();
+        }
+
+        public static string GetValue(string key)
+        {
+            return _configuration[key] ?? throw new ArgumentException($"Configuration key '{key}' not found.");
+        }
+    }
+}
